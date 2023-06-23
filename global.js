534b4432827bbf411c7670e21b17effce20d78db
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-06-23 03:14:34 -0300

    first commit

---
+++ global.js
@@ -0,0 +1,22 @@
+// Global objects
+
+////////////////////////////////////////////////////////////
+// Many available objects inside Nodejs
+console.log(global);
+
+////////////////////////////////////////////////////////////
+// global setTimeout runs once after 3 seconds
+// No need to start with 'global.'
+//global.setTimeout(() => { 
+setTimeout(() => {  
+    console.log('messaging in the timeout');
+    clearInterval(interval) // ends below loop
+}, 3000); 
+
+////////////////////////////////////////////////////////////
+// global setInterval runs every 1 second
+const interval = setInterval(() => {
+   console.log('messaging in the interval'); 
+}, 1000);
+
+// To run on terminal: node global
