7ae753f41a0e17f65c8c414864a4398475611acd
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-06-26 23:12:39 -0300

    Add global object methods __dirname and __filename

--- global.js
+++ global.js
@@ -1,22 +1,29 @@
 // The Global object and the several methods attached to it
+// Run on: Terminal>New Terminal>node global
 
-////////////////////////////////////////////////////////////
-// Available methods inside Nodejs Global objetc
+/* Available methods inside Nodejs Global object */ 
 console.log(global);
 
-////////////////////////////////////////////////////////////
-// global setTimeout runs once after 3 seconds
-// No need to start with 'global.'
-//global.setTimeout(() => { 
+/*
+ * Global setTimeout runs once after 3 seconds
+ * No need to start with 'global.'
+ * global.setTimeout(() => { 
+ */
 setTimeout(() => {  
     console.log('messaging in the timeout');
     clearInterval(interval) // ends below loop
 }, 3000); 
 
-////////////////////////////////////////////////////////////
-// global setInterval runs every 1 second
+// Global setInterval runs every 1 second
 const interval = setInterval(() => {
    console.log('messaging in the interval'); 
 }, 1000);
 
-// To run on terminal: node global
+// Returns the absolute path of the current folder that this file is in
+console.log(__dirname)
+
+// Returns the absolute path of the folder with the file name at the end
+console.log(__filename)
+
+// Returns an error because document is inside 'window', not node global
+// console.log(document.querySelector)
