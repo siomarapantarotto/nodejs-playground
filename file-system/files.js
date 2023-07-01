028e34a6b2f123917cf24b0ae395fc3d50336c94
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-06-30 22:07:26 -0300

    Organize files, clean code and add documentation

--- file-system/files.js
+++ file-system/files.js
@@ -2,39 +2,40 @@
 const fs = require('fs')
 
 /**
- * Reading file
- * Takes 2 parameters:
+ * Reading file takes 2 parameters:
  *  1: relative path to the file
  *  2: callback function that will be fired after 1 is complete
  * Reading is assyncronous - takes some time to read the file
  */
-/*
-// fs.readFile('./resources/book12.txt', (err, data) => { // test for error caption
-fs.readFile('./resources/book1.txt', (err, data) => {
+ 
+fs.readFile('./docs/doc1.txt', (err, data) => {
     if (err) {
-        console.log(err)
+        // Try a non-existing file for error caption (doc9.txt)
+        console.log(err) 
     }
-    console.log(data.toString()) // will be printed before the book
+    console.log(data.toString()) 
 })
-console.log('last line')
-*/
+// Assyncronous - will be printed before reading the doc
+console.log('last line - logged after reading file - printed before')
+
 
 /**
- * Writing files
- * Takes 3 parameters:
+ * Writing files takes 3 parameters:
  *  1: relative path to the file
  *  2: new content that will replace the content of the file
- *  3: callback function
+ *  3: callback function that will be fired after 2 is complete
  * Writing is assyncronous - takes some time to write to the file
- */ 
-/* fs.writeFile('./resources/book1.txt', 'Hello World!', () => {
+ */
+
+// This will change the entire file content to 'Hello World'
+fs.writeFile('./docs/doc1.txt', 'Hello World!', () => {
     console.log('file was written')
 })
-// Creates the file if it doesn't exist
-fs.writeFile('./resources/book2.txt', 'Hello World Again!', () => {
+// This creates the file if it doesn't exist
+fs.writeFile('./docs/doc2.txt', 'Hello World Again!', () => {
     console.log('file was written')
 }) 
-*/
+
 
 /**
  * Create and delete directories
@@ -42,21 +43,23 @@ fs.writeFile('./resources/book2.txt', 'Hello World Again!', () => {
  *  1: relative path to the directory
  *  2: callback function
  */
-if (!fs.existsSync('./modules-and-require/assets')) {
-    fs.mkdir('./modules-and-require/assets', (err) => {
+ 
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
         if (err) {
             console.log(err)
         }
         console.log('folder assets created')
     })
 } else {
-    fs.rmdir('./modules-and-require/assets', (err) => {
+    fs.rmdir('./assets', (err) => {
         if (err) {
             console.log(err)
         }
         console.log('folder assets removed')
     })
 }
+
 
 /**
  * Deleting files
@@ -65,8 +68,9 @@ if (!fs.existsSync('./modules-and-require/assets')) {
  *  2: callback function
  * Create file delete-me1.txt inside garbage folder before testing
  */
-if(fs.existsSync('./modules-and-require/garbage/delete-me1.txt')) {
-    fs.unlink('./modules-and-require/garbage/delete-me1.txt', (err) => {
+
+if(fs.existsSync('./garbage/delete-me1.txt')) {
+    fs.unlink('./garbage/delete-me1.txt', (err) => {
         if (err) {
             console.log(err)
         }
